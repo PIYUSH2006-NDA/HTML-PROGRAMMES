8b6f24e595a0349c5dd822e5b04b9bbe2e3b1940
PIYUSH2006-NDA <piyushvarule15@gmail.com>
2024-10-16 23:41:20 +0530

    calculation of the student average marks using J.S

---
+++ calculation of the student average marks using J.s
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>avg of the student</title>
+    <center><b><i><font face="Algerian">calculate the student avg. marks</i></b></center>
+    <script language="JavaScript">
+    var m1,m2,m3,avg;
+    m1=eval(prompt("enter marks in first subject"));
+    m2=eval(prompt("enter marks in second subject"));
+    m3=eval(prompt("enter marks in third subject"));
+    avg=(m1+m2+m3)/3;
+    document.write("the avg of the marks is:-"+avg);
+    </script>
+    </head>
+    </html>
